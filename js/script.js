1e4b8c6a7c6054794155c42f3d5e546ce6f5860e
Amir Mahdi Kahdouii <Amirmahdikahdouii@users.noreply.github.com>
2022-04-10 21:58:05 +0430

    header section now is as a complete independent secton. dashboard.html created but not complete.

--- js/script.js
+++ js/script.js
@@ -1,21 +1,3 @@
-// hamburgur NavBar
-let headerMenuBotton = document.getElementById('header-nav-menu-icon');
-let menuCloseButton = document.getElementById('header-nav-menu-close-icon');
-let headerMenu = document.getElementById('header-menu-id');
-headerMenuBotton.addEventListener('click', () => {
-    headerMenu.style.display = 'flex';
-    menuCloseButton.style.display = 'block';
-    headerMenuBotton.style.display = 'none';
-})
-
-menuCloseButton.addEventListener('click', () => {
-    headerMenu.style.display = 'none';
-    headerMenuBotton.style.display = 'block';
-    menuCloseButton.style.display = 'none';
-})
-
-
-
 // our-full-menu-header-button
 
 let menuShow = number => {
