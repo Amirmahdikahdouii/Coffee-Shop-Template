ce5ab0c7bd0d7ad431bc942320bb397509e2be6d
Amir Mahdi Kahdouii <Amirmahdikahdouii@users.noreply.github.com>
2022-04-17 17:55:04 +0430

    Dashbord.html -> make factors section, change border in login.css and register.css and make some changes in dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -14,3 +14,25 @@ showContentHandler = (contentId, tagId) => {
     let tag = document.getElementById(tagId);
     tag.className = 'sideBar-menu-item-active';
 }
+
+// Factors According
+
+let factorsAccordingsOpenButtons = [...document.getElementsByClassName('factor-item-menu-botton-open')];
+let factorsAccordingsCloseButtons = [...document.getElementsByClassName('factor-item-menu-botton-close')];
+let factorAccordingContainers = [...document.getElementsByClassName('factor-item-according-container')];
+
+factorsAccordingsOpenButtons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+        button.style.display = 'none';
+        factorsAccordingsCloseButtons[index].style.display = 'block';
+        factorAccordingContainers[index].style.maxHeight =  factorAccordingContainers[index].scrollHeight + "px";
+    })
+});
+
+factorsAccordingsCloseButtons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+        button.style.display = 'none';
+        factorsAccordingsOpenButtons[index].style.display = 'block';
+        factorAccordingContainers[index].style.maxHeight = null;
+    })
+})
