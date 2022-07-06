51aed1f9c77e222ea93153420402a997918057f0
amirmahdikahdouii <amirmahdikahdooi@gmail.com>
2022-07-07 00:20:21 +0430

    make sign-up.js for send and verify form data, work with ajax and send form data like a HTML form

---
+++ js/sign-up.js
@@ -0,0 +1,38 @@
+let submitButton = document.querySelector(".submit-form-button");
+submitButton.addEventListener("click", (event) => {
+    let usernameInput = document.getElementById("username");
+    let emailInput = document.getElementById("email");
+    let passwordInput = document.getElementById("password");
+    let confirmPasswordInput = document.getElementById("confirm-password");
+    let usernameInputValue = usernameInput.value.trim();
+    let emailInputValue = emailInput.value.trim();
+    let passwordInputValue = passwordInput.value;
+    let confirmPasswordInputValue = confirmPasswordInput.value;
+    let emailValidationRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    const stopEvent = (message) => {
+        alert(message);
+        event.preventDefault();
+        return null;
+    }
+    const checkLengthOfInputValues = (inputValue, validLength) => {
+        if (inputValue.length > validLength) {
+            stopEvent("Email value Should be maximum 50 character");
+        }
+    }
+    checkLengthOfInputValues(usernameInputValue, 50);
+    if (!emailInputValue.match(emailValidationRegex)) {
+        stopEvent("Please enter valid E-Mail");
+    }
+    checkLengthOfInputValues(emailInputValue, 50);
+    if (passwordInputValue !== confirmPasswordInputValue) {
+        stopEvent("Password Field and Confirm Password Field Values are not match!");
+    }
+    checkLengthOfInputValues(passwordInputValue, 80);
+    let xHttpRequest = new XMLHttpRequest();
+    xHttpRequest.onload = () => {
+        alert("Done");
+    }
+    xHttpRequest.open("POST", "your address");
+    xHttpRequest.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xHttpRequest.send(`username=${usernameInputValue}&email=${emailInputValue}&password=${passwordInputValue}`);
+})
